e72afc1518b265466e05dfaac7ff3129c9f430d1
michael seaman <mseaman26@gmail.com>
2023-04-18 23:50:23 -0700

    hamburger on right side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom'
+import { slide as Menu } from 'react-burger-menu';
+
+
 
  
 
 const Header = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function handleResize() {
+          setWindowWidth(window.innerWidth);
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
+      const showMenu = windowWidth < 768;
+
     return(
         <div className='container'>
             <div className='row'>
@@ -18,7 +35,24 @@ const Header = () => {
                         <FontAwesomeIcon icon={faLinkedin} />
                     </a> 
                 </div>
-                <div className='col-6 d-flex align-items-center justify-content-end'>
+                {showMenu ? (
+                    <Menu className='col-6' right>
+                    <a className="menu-item" href="/">
+                      Home
+                    </a>
+                    <a className="menu-item" href="/salads">
+                      Salads
+                    </a>
+                    <a className="menu-item" href="/pizzas">
+                      Pizzas
+                    </a>
+                    <a className="menu-item" href="/desserts">
+                      Desserts
+                    </a>
+                  </Menu>
+                   
+                ) : (
+                    <div className='col-6 d-flex align-items-center justify-content-end'>
                     <Link className='icon_link'>
                         Projects
                     </Link>
@@ -29,6 +63,8 @@ const Header = () => {
                         Contact
                     </Link>
                 </div>
+                )}
+                
             </div>
         </div>
     )
