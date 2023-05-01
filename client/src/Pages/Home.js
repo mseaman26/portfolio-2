19f0775263b2d85d54cf61ced39f3fc5c3ec4f05
michael seaman <mseaman26@gmail.com>
2023-05-01 13:57:59 -0700

    work on home page and styling

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,10 +4,21 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div className='home_page page'>
-            <h1>
-                Home Page
+            <h1 className='main_header'>
+                Michael Seaman
             </h1>
-            <Link to='/projects'>Projects</Link>
+            <h2>
+                ____
+            </h2>
+            <h2 className='secondayr_header'>
+                Full-Stack Developer
+            </h2>
+            <h2>
+                ____
+            </h2>
+            <p>
+            React | JavaScript | MERN Stack | GraphQL | ES6 Express | Node | MongoDB | MySQL | CSS | HTML 5 | REST | GitHub | UX/UI | MVC | Relational Databases | Strong Communication | Problem-Solving | Responsive Web Design | Developer Tools | Insomnia | Creating Server-Side API | Third-Party API | Fetch | AJAX | Wireframe Creation | Git | GitHub | TDD | Jest | and more…
+            </p>
         </div>
         
     )
