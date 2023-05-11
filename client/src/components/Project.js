eccdd138a4c90fd7880ed9bb451f205b41b22916
michael seaman <mseaman26@gmail.com>
2023-05-11 15:42:21 -0700

    projects have links now

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -6,10 +6,10 @@ const Project = (props) => {
             <div className="project_link row">
                 <div className="col-lg-6 col-sm-12 project_clickable">
                     <h3>{props.project.name}</h3>
-                    <img className="project_image" src={props.project.image} alt={props.project.name}/>
+                    <a href={props.project.link} target="_blank" rel="noreferrer"><img className="project_image" src={props.project.image} alt={props.project.name}/></a>
                 </div>
-                <div className="col-lg-6 col-sm-12">
-                    {/* <h3 className="tech_header">Technologies Used</h3> */}
+                <div className="col-lg-6 col-sm-12 tech_div">
+                    <h3 className="tech_header">Technologies Used</h3>
                     <br/>
                     <ul className="tech_list">
                         {props.project.techs ? 
@@ -21,6 +21,10 @@ const Project = (props) => {
                             )
                         }) : <></>}
                     </ul>
+                </div>
+                <div className="container project_button_div">
+                    <a href={props.project.link} target="_blank" rel="noreferrer"><button className="btn btn-success project_button">Visit live Site</button></a>
+                    <a href={props.project.gitHub} target="_blank" rel="noreferrer"><button className="btn btn-success project_button">Visit Repo</button></a>
                 </div>
             </div>
             
